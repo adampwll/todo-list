c9e181b2d5f643ec897a4360056da5d70972223f
adampwll <adampwll96@gmail.com>
2025-07-15 11:08:22 -0400

    Added stubbed response to TodoController

--- todo-backend/todo-backend/src/main/java/com/powell/todo_backend/controller/TodoController.java
+++ todo-backend/todo-backend/src/main/java/com/powell/todo_backend/controller/TodoController.java
@@ -1,4 +1,13 @@
 package com.powell.todo_backend.controller;
 
+import com.powell.todo_backend.model.TodoListItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TodoController {
+    @GetMapping("/todo/")
+    public TodoListItem getTodos() {
+        return new TodoListItem(3, "test", false);
+    }
 }
